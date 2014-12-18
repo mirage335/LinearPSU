fef649ef6f9c0c6fc9a815395552472865a5793b
mirage335 <spamfreemirage335@gmail.com>
2014-12-18 05:50:46 +0000

    first commit

---
+++ OutputRegulator.cmd
@@ -0,0 +1,353 @@
+# Pin name action command file
+
+# Start of element CONN7
+ChangePinName(CONN7, 2, 2)
+ChangePinName(CONN7, 1, 1)
+
+# Start of element C8
+ChangePinName(C8, 2, -)
+ChangePinName(C8, 1, +)
+
+# Start of element C7
+ChangePinName(C7, 2, -)
+ChangePinName(C7, 1, +)
+
+# Start of element C6
+ChangePinName(C6, 2, -)
+ChangePinName(C6, 1, +)
+
+# Start of element D8
+ChangePinName(D8, 1, 1)
+ChangePinName(D8, 2, 2)
+
+# Start of element D7
+ChangePinName(D7, 1, 1)
+ChangePinName(D7, 2, 2)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element C5
+ChangePinName(C5, 2, -)
+ChangePinName(C5, 1, +)
+
+# Start of element R5
+ChangePinName(R5, 1, 1)
+ChangePinName(R5, 2, 2)
+
+# Start of element R6
+ChangePinName(R6, 1, ccw)
+ChangePinName(R6, 3, cw)
+ChangePinName(R6, 2, wiper)
+
+# Start of element U2
+ChangePinName(U2, 1, Adjust)
+ChangePinName(U2, 3, Vin)
+ChangePinName(U2, 2, Vout)
+
+# Start of element C18
+ChangePinName(C18, 2, -)
+ChangePinName(C18, 1, +)
+
+# Start of element C19
+ChangePinName(C19, 2, 2)
+ChangePinName(C19, 1, 1)
+
+# Start of element C20
+ChangePinName(C20, 2, 2)
+ChangePinName(C20, 1, 1)
+
+# Start of element 3.3V
+ChangePinName(3.3V, 3, OUT)
+ChangePinName(3.3V, 1, GND)
+ChangePinName(3.3V, 2, IN)
+
+# Start of element 5V
+ChangePinName(5V, 3, 3)
+ChangePinName(5V, 2, 2)
+ChangePinName(5V, 1, 1)
+
+# Start of element C17
+ChangePinName(C17, 2, -)
+ChangePinName(C17, 1, +)
+
+# Start of element C13
+ChangePinName(C13, 2, -)
+ChangePinName(C13, 1, +)
+
+# Start of element D15
+ChangePinName(D15, 1, 1)
+ChangePinName(D15, 2, 2)
+
+# Start of element D14
+ChangePinName(D14, 1, 1)
+ChangePinName(D14, 2, 2)
+
+# Start of element R18
+ChangePinName(R18, 1, 1)
+ChangePinName(R18, 2, 2)
+
+# Start of element R19
+ChangePinName(R19, 1, 1)
+ChangePinName(R19, 2, 2)
+
+# Start of element U6
+ChangePinName(U6, 1, Adjust)
+ChangePinName(U6, 3, Vin)
+ChangePinName(U6, 2, Vout)
+
+# Start of element C1
+ChangePinName(C1, 2, -)
+ChangePinName(C1, 1, +)
+
+# Start of element C11
+ChangePinName(C11, 2, -)
+ChangePinName(C11, 1, +)
+
+# Start of element C4
+ChangePinName(C4, 2, -)
+ChangePinName(C4, 1, +)
+
+# Start of element C10
+ChangePinName(C10, 2, -)
+ChangePinName(C10, 1, +)
+
+# Start of element C15
+ChangePinName(C15, 2, 2)
+ChangePinName(C15, 1, 1)
+
+# Start of element C16
+ChangePinName(C16, 2, -)
+ChangePinName(C16, 1, +)
+
+# Start of element C2
+ChangePinName(C2, 2, -)
+ChangePinName(C2, 1, +)
+
+# Start of element L2
+ChangePinName(L2, 1, 1)
+ChangePinName(L2, 2, 2)
+
+# Start of element L1
+ChangePinName(L1, 1, 1)
+ChangePinName(L1, 2, 2)
+
+# Start of element RJ45
+ChangePinName(RJ45, 9, 9)
+ChangePinName(RJ45, 8, 8)
+ChangePinName(RJ45, 7, 7)
+ChangePinName(RJ45, 6, 6)
+ChangePinName(RJ45, 5, 5)
+ChangePinName(RJ45, 4, 4)
+ChangePinName(RJ45, 3, 3)
+ChangePinName(RJ45, 2, 2)
+ChangePinName(RJ45, 1, 1)
+
+# Start of element D6
+ChangePinName(D6, 1, 1)
+ChangePinName(D6, 2, 2)
+
+# Start of element D16
+ChangePinName(D16, 1, 1)
+ChangePinName(D16, 2, 2)
+
+# Start of element D13
+ChangePinName(D13, 1, 1)
+ChangePinName(D13, 2, 2)
+
+# Start of element D12
+ChangePinName(D12, 1, 1)
+ChangePinName(D12, 2, 2)
+
+# Start of element D11
+ChangePinName(D11, 1, 1)
+ChangePinName(D11, 2, 2)
+
+# Start of element D10
+ChangePinName(D10, 1, 1)
+ChangePinName(D10, 2, 2)
+
+# Start of element D3
+ChangePinName(D3, 1, 1)
+ChangePinName(D3, 2, 2)
+
+# Start of element D5
+ChangePinName(D5, 1, 1)
+ChangePinName(D5, 2, 2)
+
+# Start of element D4
+ChangePinName(D4, 1, 1)
+ChangePinName(D4, 2, 2)
+
+# Start of element D2
+ChangePinName(D2, 1, 1)
+ChangePinName(D2, 2, 2)
+
+# Start of element D1
+ChangePinName(D1, 2, 2)
+ChangePinName(D1, 1, 1)
+
+# Start of element R13
+ChangePinName(R13, 1, 1)
+ChangePinName(R13, 2, 2)
+
+# Start of element R14
+ChangePinName(R14, 1, 1)
+ChangePinName(R14, 2, 2)
+
+# Start of element CONN6
+ChangePinName(CONN6, 3, 3)
+ChangePinName(CONN6, 2, 2)
+ChangePinName(CONN6, 1, 1)
+
+# Start of element F1
+ChangePinName(F1, 2, 2)
+ChangePinName(F1, 1, 1)
+
+# Start of element F4
+ChangePinName(F4, 2, 2)
+ChangePinName(F4, 1, 1)
+
+# Start of element F3
+ChangePinName(F3, 2, 2)
+ChangePinName(F3, 1, 1)
+
+# Start of element F2
+ChangePinName(F2, 2, 2)
+ChangePinName(F2, 1, 1)
+
+# Start of element CONN2
+ChangePinName(CONN2, 6, 6)
+ChangePinName(CONN2, 5, 5)
+ChangePinName(CONN2, 4, 4)
+ChangePinName(CONN2, 3, 3)
+ChangePinName(CONN2, 2, 2)
+ChangePinName(CONN2, 1, 1)
+
+# Start of element C3
+ChangePinName(C3, 2, -)
+ChangePinName(C3, 1, +)
+
+# Start of element CONN1
+ChangePinName(CONN1, 2, 2)
+ChangePinName(CONN1, 1, 1)
+
+# Start of element CONN3
+ChangePinName(CONN3, 2, 2)
+ChangePinName(CONN3, 1, 1)
+
+# Start of element CONN5
+ChangePinName(CONN5, 3, 3)
+ChangePinName(CONN5, 2, 2)
+ChangePinName(CONN5, 1, 1)
+
+# Start of element CONN4
+ChangePinName(CONN4, 2, 2)
+ChangePinName(CONN4, 1, 1)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element U1
+ChangePinName(U1, 1, Adjust)
+ChangePinName(U1, 3, Vin)
+ChangePinName(U1, 2, Vout)
+
+# Start of element R15
+ChangePinName(R15, 1, 1)
+ChangePinName(R15, 2, 2)
+
+# Start of element R17
+ChangePinName(R17, 1, 1)
+ChangePinName(R17, 2, 2)
+
+# Start of element R16
+ChangePinName(R16, 1, 1)
+ChangePinName(R16, 2, 2)
+
+# Start of element Q2
+ChangePinName(Q2, 2, collector)
+ChangePinName(Q2, 3, emitter)
+ChangePinName(Q2, 1, base)
+
+# Start of element Q1
+ChangePinName(Q1, 2, collector)
+ChangePinName(Q1, 3, emitter)
+ChangePinName(Q1, 1, base)
+
+# Start of element U5
+ChangePinName(U5, 5, s)
+ChangePinName(U5, 6, Vo)
+ChangePinName(U5, 4, Vee)
+ChangePinName(U5, 7, Vcc)
+ChangePinName(U5, 2, -)
+ChangePinName(U5, 3, +)
+
+# Start of element C14
+ChangePinName(C14, 2, -)
+ChangePinName(C14, 1, +)
+
+# Start of element C12
+ChangePinName(C12, 2, -)
+ChangePinName(C12, 1, +)
+
+# Start of element U4
+ChangePinName(U4, 1, Adjust)
+ChangePinName(U4, 3, Vin)
+ChangePinName(U4, 2, Vout)
+
+# Start of element R8
+ChangePinName(R8, 1, 1)
+ChangePinName(R8, 2, 2)
+
+# Start of element C9
+ChangePinName(C9, 2, -)
+ChangePinName(C9, 1, +)
+
+# Start of element R9
+ChangePinName(R9, 1, 1)
+ChangePinName(R9, 2, 2)
+
+# Start of element R7
+ChangePinName(R7, 1, 1)
+ChangePinName(R7, 2, 2)
+
+# Start of element R12
+ChangePinName(R12, 1, 1)
+ChangePinName(R12, 2, 2)
+
+# Start of element R11
+ChangePinName(R11, 1, 1)
+ChangePinName(R11, 2, 2)
+
+# Start of element C21
+ChangePinName(C21, 2, -)
+ChangePinName(C21, 1, +)
+
+# Start of element C22
+ChangePinName(C22, 2, -)
+ChangePinName(C22, 1, +)
+
+# Start of element D9
+ChangePinName(D9, 2, 2)
+ChangePinName(D9, 1, 1)
+
+# Start of element R10
+ChangePinName(R10, 1, ccw)
+ChangePinName(R10, 3, cw)
+ChangePinName(R10, 2, wiper)
+
+# Start of element U3
+ChangePinName(U3, 1, Adjust)
+ChangePinName(U3, 3, Vin)
+ChangePinName(U3, 2, Vout)
---
+++ customParts/footprints/TRA3M.fp
@@ -0,0 +1,15 @@
+
+Element["" "" "" "" 623.00mil 4367.00mil 0.0000 0.0000 0 100 ""]
+(
+	Pin[-250.00mil -64.00mil 63.00mil 20.00mil 69.00mil 31.00mil "" "1" "edge2"]
+	Pin[-360.00mil 0.0000 63.00mil 20.00mil 69.00mil 31.00mil "" "2" "edge2"]
+	Pin[-250.00mil 62.00mil 63.00mil 20.00mil 69.00mil 31.00mil "" "3" "edge2"]
+	Pin[0.0000 200.00mil 160.00mil 20.00mil 166.00mil 128.00mil "" "4" "edge2"]
+	Pin[0.0000 -200.00mil 160.00mil 20.00mil 166.00mil 128.00mil "" "5" "edge2"]
+	Pin[0.0000 0.0000 102.00mil 20.00mil 108.00mil 70.00mil "" "6" "edge2"]
+	ElementLine [-474.00mil -302.00mil 310.00mil -302.00mil 10.00mil]
+	ElementLine [-474.00mil 303.00mil -474.00mil -302.00mil 10.00mil]
+	ElementLine [310.00mil 303.00mil -474.00mil 303.00mil 10.00mil]
+	ElementLine [310.00mil 303.00mil 310.00mil -302.00mil 10.00mil]
+
+	)
